dd5cfd191705fe8a0f1c5524894ade536401f9fc
Muhammed Dilmaç <73828688+muhammeddilmacc@users.noreply.github.com>
2022-10-12 00:19:34 +0300

    Master | completing mongodb

--- 11.Mongodb/models/user.js
+++ 11.Mongodb/models/user.js
@@ -103,8 +103,9 @@ class User {
   }
 
   getOrders() {
-    const db = getDB();
-    // return db.collection('orders').
+    const db = getDb();
+    return db.collection('orders').find({ 'user._id': new ObjectId(this._id) }).toArray();
+
   }
 
   static findById(userId) {
