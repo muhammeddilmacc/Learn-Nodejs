c6b73a3e43de51cbd539d062649085c1a07b9c2d
Muhammed Dilmaç <73828688+muhammeddilmacc@users.noreply.github.com>
2022-10-13 19:33:03 +0300

    Basic Authentication

--- 13.Sessions-and-cookies/controllers/auth.js
+++ 13.Sessions-and-cookies/controllers/auth.js
@@ -1,6 +1,16 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn =  req.get('Cookie').split('=')[1].trim().split()[0];
+  console.log(req.get('Cookie'))
+  console.log(isLoggedIn, typeof isLoggedIn);
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    isAuthenticated: isLoggedIn
   });
+};
+
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true');
+  res.redirect('/');
 };
