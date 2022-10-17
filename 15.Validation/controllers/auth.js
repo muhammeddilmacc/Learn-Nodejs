1017884d087ae0f72dd66fa163ef2a3ce20a8c05
Muhammed Dilmaç <73828688+muhammeddilmacc@users.noreply.github.com>
2022-10-17 21:59:13 +0300

    change place of condition (mail already in use )

--- 15.Validation/controllers/auth.js
+++ 15.Validation/controllers/auth.js
@@ -80,7 +80,6 @@ exports.postLogin = (req, res, next) => {
 exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.confirmPassword;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).render('auth/signup', {
@@ -89,34 +88,25 @@ exports.postSignup = (req, res, next) => {
       errorMessage: errors.array()[0].msg
     })
   }
-  User.findOne({ email: email })
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'this email is already in use');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          transporter.sendMail({
-            from: 'mtermux@gmail.com',
-            to: email,
-            subject: 'Sending Email using Node.js',
-            html: '<h3>You successfully signed up!</h3>'
-          });
-        })
-        .catch(err => {
-          console.log(err);
-        });
+
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPassword => {
+      const user = new User({
+        email: email,
+        password: hashedPassword,
+        cart: { items: [] }
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+      transporter.sendMail({
+        from: 'mtermux@gmail.com',
+        to: email,
+        subject: 'Sending Email using Node.js',
+        html: '<h3>You successfully signed up!</h3>'
+      });
     })
     .catch(err => {
       console.log(err);
--- 15.Validation/routes/auth.js
+++ 15.Validation/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { check, body } = require('express-validator/check');
-
 const authController = require('../controllers/auth');
+const User = require('../models/user');
 
 const router = express.Router();
 
@@ -16,15 +16,29 @@ router.post('/signup',
         .isEmail()
         .withMessage('Please enter a valid email')
         .custom((value, { req }) => {
-            if (value === 'mtermux@gmail.com') {
-                throw new Error('This email address is forbiden.');
-            }
-            return true;
+            // if (value === 'mtermux@gmail.com') {
+            //     throw new Error('This email address is forbiden.');
+            // }
+            // return true;
+            return User.findOne({ email: value })
+                .then(userDoc => {
+                    if (userDoc) {
+                        return Promise.reject('This email address is already in use.');
+                    }
+                });
+
         }),
 
     body('password')
         .isLength({ min: 8 })
-        .withMessage('Password must be at least 8 characters long')]
+        .withMessage('Password must be at least 8 characters long'),
+    body('confirmPassword')
+        .custom((value, { req }) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords does not macth');
+            }
+            return true;
+        })]
     , authController.postSignup);
 
 router.post('/logout', authController.postLogout);
